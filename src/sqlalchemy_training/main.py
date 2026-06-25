3daad99a9bd0d1f917435e3ccb4c86274712fb1b
Poy <heavencube@tuta.io>
2026-06-25 15:44:32 +0200

    Correction des commentaires et des messages de debug, mise à jour des fonctions d'ajout de téléphones et d'utilisateurs

--- src/sqlalchemy_training/main.py
+++ src/sqlalchemy_training/main.py
@@ -1,8 +1,8 @@
-# -> Importation du module sqlite3 pour interagir avec la base de données SQLite en local
+# -> Importation du module sqlite3 pour interagir avec une base de données SQLite locale
 import sqlite3
 
 
-# -> Création de la table "telephone" (si elle n'existe pas déjà)
+# -> Création de la table "telephone" si elle n'existe pas déjà
 def db_create_telephone(db_curseur):
     db_curseur.execute("""
     CREATE TABLE IF NOT EXISTS telephone (
@@ -11,31 +11,30 @@ def db_create_telephone(db_curseur):
         modele TEXT NOT NULL,
         prix INTEGER NOT NULL,
         UNIQUE(marque, modele)
-    )""")
+    )
+    """)
 
 
-# -> Création de la table "utilisateur" (si elle n'existe pas déjà)
+# -> Création de la table "utilisateur" si elle n'existe pas déjà
 def db_create_utilisateur(db_curseur):
     db_curseur.execute("""
     CREATE TABLE IF NOT EXISTS utilisateur (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nom TEXT NOT NULL,
-        email TEXT NOT NULL,
-        UNIQUE(email)
-    )""")
+        email TEXT NOT NULL UNIQUE
+    )
+    """)
 
 
-# -> Insertion de téléphones
-def db_ajouter_telephones(db_curseur, marque, modele, prix):
+# -> Insertion d'un téléphone
+def db_ajouter_telephone(db_curseur, marque, modele, prix):
     db_curseur.execute("""
     INSERT OR IGNORE INTO telephone (marque, modele, prix)
     VALUES (?, ?, ?)
     """, (marque, modele, prix))
 
-    # print(f"Nombre de téléphones ajoutés dans la base de données : {db_curseur.rowcount}.")
 
-
-# -> Affichage des téléphones
+# -> Affichage des téléphones dont le prix est supérieur à un prix minimum
 def db_afficher_telephones(db_curseur, prix_minimum):
     db_curseur.execute("""
     SELECT id, marque, modele, prix
@@ -48,7 +47,7 @@ def db_afficher_telephones(db_curseur, prix_minimum):
 
     for query_telephone in db_telephones:
         id_telephone, marque, modele, prix = query_telephone
-        print(f"[DEBUG: PHONE] ID: {id_telephone} • Le téléphone \"{marque} {modele}\" coûte {prix}€.")
+        print(f"[DEBUG: PHONE] ID: {id_telephone} • Le téléphone \"{marque} {modele}\" coûte {prix} €.")
 
 
 # -> Insertion d'un utilisateur
@@ -57,8 +56,6 @@ def db_ajouter_utilisateur(db_curseur, nom, email):
     INSERT OR IGNORE INTO utilisateur (nom, email)
     VALUES (?, ?)
     """, (nom, email))
-
-    # print(f"Nombre d'utilisateurs ajoutés dans la base de données : {db_curseur.rowcount}.")
 
 
 # -> Affichage des utilisateurs
@@ -72,20 +69,21 @@ def db_afficher_utilisateurs(db_curseur):
 
     for query_utilisateur in db_utilisateurs:
         id_utilisateur, nom, email = query_utilisateur
-        print(f"[DEBUG: USER] ID: {id_utilisateur} L'utilisateur \"{nom}\" a pour email \"{email}\".")
+        print(f"[DEBUG: USER] ID: {id_utilisateur} • L'utilisateur \"{nom}\" a pour email \"{email}\".")
 
 
 # -> Fonction principale du programme
 def main():
     db_connexion = sqlite3.connect("sqlalchemy_training.db")
+    db_connexion.execute("PRAGMA foreign_keys = ON")
     db_curseur = db_connexion.cursor()
 
     db_create_telephone(db_curseur)
     db_create_utilisateur(db_curseur)
 
-    db_ajouter_telephones(db_curseur, "Samsung", "Galaxy A55", 399)
-    db_ajouter_telephones(db_curseur, "Samsung", "Galaxy Z Flip6", 1199)
-    db_afficher_telephones(db_curseur, 0) # -> Affiche les téléphones dont le prix est supérieur à 0€
+    db_ajouter_telephone(db_curseur, "Samsung", "Galaxy A55", 399)
+    db_ajouter_telephone(db_curseur, "Samsung", "Galaxy Z Flip6", 1199)
+    db_afficher_telephones(db_curseur, 0)
 
     db_ajouter_utilisateur(db_curseur, "Paul", "contact@paulmuller.dev")
     db_afficher_utilisateurs(db_curseur)
@@ -94,5 +92,6 @@ def main():
     db_connexion.close()
 
 
-# -> Exécution de la fonction principale si le script est exécuté directement ;
-main()
+# -> Exécution de la fonction principale uniquement si ce fichier est lancé directement
+if __name__ == "__main__":
+    main()
